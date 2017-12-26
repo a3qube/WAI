13213f205d3e263d0a1c89154d71c5966ba0f306
Arjun Nair <aqube@Arjuns-MacBook-Air.local>
2017-12-25 21:06:07 -0800

    Error correction and header styling r Please enter the commit message for your changes. Lines starting

--- src/app/headerValues.ts
+++ src/app/headerValues.ts
@@ -2,7 +2,7 @@ import {header} from './header';
 export const headerValues : header = {
 	name : 'Arjun A Nair',
 	designation : 'UI/UX Enthusiaist',
-	quote : 'Stay hungry, stay foolish.',
+	quote : 'Stay hungry, stay foolish',
 	author : 'Steve Jobs'
 
 }
--- src/app/params-list/list-param-values.ts
+++ src/app/params-list/list-param-values.ts
@@ -1,27 +1,39 @@
 import {listParams} from './list-params';
 import {desc} from './details';
 export const AllValues : listParams[] = [
-	{param : 'Objective', 
-	sequence : 1,
-	details : [{value:'A Front-end enthusiast with 7+ years of experience in UI/UX designing. Worked with Manufacturing-Retail, Consumer Electronics, Energy and Utilities, Pharma domains. I also had an opportunity to work with the ERP giant SAP, for a from the scratch development product. I am ardent learner of new technologies and the evolution has been successful in inspiring me all the time. I always design to make things work with the right way, which rewards with ease of maintenance and flexibility of enhancements.'}]},
-	{param : 'Expirience', 
-	sequence : 2
-	details : [{value:"Expirience goes here"},
-				{value:"Expirience1 goes here"}]},
-	{param : 'Education', 
-	sequence : 3
-	details : [{value:"Education goes here"},
-				{value:"Education1 goes here"}]},
-	{param : 'Learning', 
-	sequence : 4
-	details : [{value:"Learning goes here"},
-				{value:"Learning1 goes here"}]},
-	{param : 'Hobbies', 
-	sequence : 5
-	details : [{value:"Hobbies goes here"},
-				{value:"Hobbies1 goes here"}]},
-	{param : 'Contact', 
-	sequence : 6
-	details : [{value:"Contact goes here"},
-				{value:"Contact1 goes here"}]}
+{
+param : 'Objective', 
+sequence : 1,
+details : [{value:'A Front-end enthusiast with 7+ years of experience in UI/UX designing. Worked with Manufacturing-Retail, Consumer Electronics, Energy and Utilities, Pharma domains. I also had an opportunity to work with the ERP giant SAP, for a from the scratch development product. I am ardent learner of new technologies and the evolution has been successful in inspiring me all the time. I always design to make things work with the right way, which rewards with ease of maintenance and flexibility of enhancements.'}]
+},
+{
+param : 'Expirience', 
+sequence : 2
+details : [{value:"Expirience goes here"},
+			{value:"Expirience1 goes here"}]
+},
+{
+param : 'Education', 
+sequence : 3,
+details : [{value:"Education goes here"},
+			{value:"Education1 goes here"}]
+},
+{
+param : 'Learning', 
+sequence : 4,
+details : [{value:"Learning goes here"},
+			{value:"Learning1 goes here"}]
+},
+{
+param : 'Hobbies', 
+sequence : 5,
+details : [{value:"Hobbies goes here"},
+			{value:"Hobbies1 goes here"}]
+},
+{
+param : 'Contact', 
+sequence : 6,
+details : [{value:"Contact goes here"},
+			{value:"Contact1 goes here"}]
+}
 ]
